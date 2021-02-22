fc420933f9a88c866951ec71c7b7963704257cc7
homuribu <iesho1119@gmail.com>
2021-02-23 00:19:14 +0900

    add scripts_backup

---
+++ scripts_backup.sh
@@ -0,0 +1,6 @@
+
+cd ./scripts_backup && curl -O "https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils@0.1/camera_utils.js"
+curl -O "https://cdn.jsdelivr.net/npm/@mediapipe/control_utils@0.1/control_utils.js"
+curl -O "https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils@0.1/drawing_utils.js"
+curl -O "https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1.1612238212/hands.js" 
+curl -O "https://docs.opencv.org/master/opencv.js"
