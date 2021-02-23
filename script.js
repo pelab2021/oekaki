45337d1120309a58fa9838be9f242133c2df875f
homuribu <iesho1119@gmail.com>
2021-02-23 17:00:26 +0900

    音程で色が変わるようにした

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-
+import { freelizer } from 'https://cdn.jsdelivr.net/npm/freelizer@1.0.0/index.min.js'
 
 const maxNumHands = 2;
 //連続だとみなす2点間の距離の上限
 const maxNorm = 200;
 
+//連続だとみなす2点間の時間の長さの上限
+const maxTime = 20;
+
+let audio_data = {
+  on: true,
+  //0:白 1:赤 ... 8:黒
+  color_index: 0
+}
+// let audio_data = { 
+//   deviation: 28.924883259925537,
+//   frequency: 1075.4271444623207,
+//   note: "C",
+//   noteFrequency: 1046.5022612023952,
+//   octave: 6,
+// }
+
+function audio_data_update(data) {
+  if (Object.keys(data).includes("frequency")) {
+    audio_data.on = true
+    audio_data.color_index = ((data.note.charCodeAt(0) - 65) + data.octave * 8) % 9;
+  } else {
+    audio_data.on = false
+  }
+}
+
+; (async () => {
+  try {
+    const { start, stop, subscribe, unsubscribe } = await freelizer()
+    start()
+    subscribe(audio_data_update)
+  } catch (error) {
+    console.error(error);
+  }
+})()
+
+
+
 
 const videoElement =
-    document.getElementsByClassName('input_video')[0];
+  document.getElementsByClassName('input_video')[0];
 const canvasElement =
-    document.getElementsByClassName('output_canvas')[0];
+  document.getElementsByClassName('output_canvas')[0];
 const controlsElement =
-    document.getElementsByClassName('control-panel')[0];
+  document.getElementsByClassName('control-panel')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
 // We'll add this to our control panel later, but we'll save it here so we can
@@ -23,97 +60,134 @@ spinner.ontransitionend = () => {
   spinner.style.display = 'none';
 };
 
-let now_line = [[],[]];
+let now_line = [[], []];
 let old_imgs = [];
 
 
-const bg_color = new cv.Scalar(0,0,0,0);
+const bg_color = new cv.Scalar(0, 0, 0, 0);
 let lines = [[], []];
+
+const colors =
+  [
+    new cv.Scalar(255, 255, 255, 255),
+    new cv.Scalar(255, 0, 0, 255),
+    new cv.Scalar(255, 165, 0, 255),
+    new cv.Scalar(255, 255, 0, 255),
+    new cv.Scalar(0, 128, 0, 255),
+    new cv.Scalar(0, 255, 255, 255),
+    new cv.Scalar(0, 0, 255, 255),
+    new cv.Scalar(128, 0, 128, 255),
+    new cv.Scalar(0, 0, 0, 255),
+  ];  // RGBA
+
+function draw_img(src, dst){
+    let channels = new cv.MatVector();
+    cv.split(src, channels);
+
+    let alpha = channels.get(3);
+    let mask = new cv.Mat();
+    cv.threshold(alpha, mask, 0, 255, cv.THRESH_BINARY);
+    src.copyTo(dst, mask);
+
+    channels.delete();
+    alpha.delete();
+    mask.delete();
+}
 
 function onResults(results) {
   // Hide the spinner.
   document.body.classList.add('loaded');
 
-
-  // let now_img = new cv.Mat(canvasElement.height, canvasElement.width, cv.CV_8UC4, bg_color);
-
   // Update the frame rate.
   // fpsControl.tick();
 
-  // Draw the overlays.
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
   canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
 
-  // now_img.data.set(canvasCtx.getImageData(0, 0, canvasElement.width, canvasElement.height).data);
+  if (audio_data.on) {
+    if (results.multiHandLandmarks && results.multiHandedness) {
+      for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+        const classification = results.multiHandedness[index];
+        const isRightHand = classification.label === 'Right';
+        const landmarks = results.multiHandLandmarks[index];
+        // drawConnectors(
+        //     canvasCtx, landmarks, HAND_CONNECTIONS,
+        //     {color: isRightHand ? '#00FF00' : '#FF0000'});
+        // drawLandmarks(canvasCtx, landmarks, {
+        //   color: isRightHand ? '#00FF00' : '#FF0000',
+        //   fillColor: isRightHand ? '#FF0000' : '#00FF00'
+        // });
+        const p = new cv.Point(landmarks[8].x * canvasElement.width, landmarks[8].y * canvasElement.height);
+        const hand_i = isRightHand ? 0 : 1;
 
-  if (results.multiHandLandmarks && results.multiHandedness) {
-    for (let index = 0; index < results.multiHandLandmarks.length; index++) {
-      const classification = results.multiHandedness[index];
-      const isRightHand = classification.label === 'Right';
-      const landmarks = results.multiHandLandmarks[index];
-      // drawConnectors(
-      //     canvasCtx, landmarks, HAND_CONNECTIONS,
-      //     {color: isRightHand ? '#00FF00' : '#FF0000'});
-      // drawLandmarks(canvasCtx, landmarks, {
-      //   color: isRightHand ? '#00FF00' : '#FF0000',
-      //   fillColor: isRightHand ? '#FF0000' : '#00FF00'
-      // });
-      const p = new cv.Point(landmarks[8].x*canvasElement.width, landmarks[8].y*canvasElement.height);
-      const nl_i = isRightHand? 0 : 1;
-      
-      const nl_len = now_line[nl_i].length;
-      if (now_line[nl_i].length > 0){
-        const sq_norm = (now_line[nl_i][nl_len - 1].x - p.x) ** 2 + (now_line[nl_i][nl_len - 1].y - p.y) ** 2;
-        if (sq_norm > maxNorm ** 2) {
-
-          // old_imgs.push(now_img);
-          // now_img.delete()
-          // now_img = null;
-          lines[nl_i].push(_.cloneDeep(now_line[nl_i]));
-          now_line[nl_i].splice(0);
-          console.log(lines)
+        const nl_len = now_line[hand_i].length;
+        if (now_line[hand_i].length > 0) {
+          const sq_norm = (now_line[hand_i][nl_len - 1].x - p.x) ** 2 + (now_line[hand_i][nl_len - 1].y - p.y) ** 2;
+          if (sq_norm > maxNorm ** 2) {
+            lines[hand_i].push(_.cloneDeep(now_line[hand_i]));
+            now_line[hand_i].splice(0);
+          }
         }
+        now_line[hand_i].push(p);
       }
-      now_line[nl_i].push(p);
     }
-
   }
 
+
+  let is_empty = true;
+  const check_empty = (line) => {
+    is_empty = is_empty && (line.length == 0);
+  };
+  lines.forEach(check_empty)
+  now_line.forEach(check_empty)
+
+  let result_image = new cv.Mat(canvasElement.height, canvasElement.width, cv.CV_8UC4, bg_color);
   let now_img = new cv.Mat(canvasElement.height, canvasElement.width, cv.CV_8UC4, bg_color);
+  if (!is_empty) {
 
-  const colors = [new cv.Scalar(0, 255, 0, 255), new cv.Scalar(255, 0, 0, 255)];  // RGBA
-
-  for (let hand_n = 0; hand_n < maxNumHands; hand_n++){
-    const drawline = (line)=>{
-      const line_points = draw_calc(line);
-      for (let i = 0; i < line_points.length - 1; i++) {
-        cv.line(now_img, line_points[i], line_points[i + 1], colors[hand_n], 3, cv.LINE_8, 0);
-      }
-    };
-    lines[hand_n].forEach(drawline);
-    drawline(now_line[hand_n])
+    for (let hand_i = 0; hand_i < maxNumHands; hand_i++) {
+      const drawline = (line) => {
+        const line_points = draw_calc(line);
+        for (let i = 0; i < line_points.length - 1; i++) {
+          cv.line(now_img, line_points[i], line_points[i + 1], colors[audio_data.color_index], 3, cv.LINE_8, 0);
+        }
+      };
+      lines[hand_i].forEach(drawline);
+      drawline(now_line[hand_i])
+    }
   }
 
-  old_imgs.forEach((o_img)=>{
-    // cv.imshow(canvasElement, o_img);
+  old_imgs.forEach((o_img) => {
+    draw_img(o_img, result_image)
   });
+  draw_img(now_img, result_image)
+  cv.imshow(canvasElement, result_image);
+  result_image.delete()
 
-  cv.imshow(canvasElement, now_img);
-  now_img.delete();
-
+  if (!audio_data.on && !is_empty) {
+    for (let hand_i = 0; hand_i < maxNumHands; hand_i++) {
+      lines[hand_i].splice(0)
+      now_line[hand_i].splice(0);
+    }
+    old_imgs.push(now_img);
+  } else {
+    now_img.delete();
+  }
   canvasCtx.restore();
 }
 
-const hands = new Hands({locateFile: (file) => {
-  // return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1.1612238212/${file}`;
-}})
+const hands = new Hands({
+  locateFile: (file) => {
+    // return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1.1612238212/${file}`;
+  }
+})
 
-options = {
-      maxNumHands: maxNumHands,
-      minDetectionConfidence: 0.7,
-      minTrackingConfidence: 0.7
+const options = {
+  maxNumHands: maxNumHands,
+  minDetectionConfidence: 0.7,
+  minTrackingConfidence: 0.7
 }
 
 hands.setOptions(options);
@@ -124,7 +198,7 @@ hands.onResults(onResults);
  */
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await hands.send({image: videoElement});
+    await hands.send({ image: videoElement });
   },
   width: 1280,
   height: 720
@@ -163,11 +237,11 @@ camera.start();
 //       hands.setOptions(options);
 //     });
 
-function draw_calc(now_line){
+function draw_calc(line) {
   let retval = []
-  if (now_line.length > 0){
+  if (line.length > 0) {
     let points2 = []
-    now_line.forEach((p) => {
+    line.forEach((p) => {
       points2.push(p.x)
       points2.push(p.y)
     })
@@ -176,7 +250,7 @@ function draw_calc(now_line){
     let close = false;
     let splinePoints = getCurvePoints(points2, tension, numOfSeg, close);
     for (let i = 0; i < splinePoints.length / 2; i++) {
-      p = new cv.Point(splinePoints[2 * i], splinePoints[2 * i + 1])
+      let p = new cv.Point(splinePoints[2 * i], splinePoints[2 * i + 1])
       retval.push(p)
     }
   }
